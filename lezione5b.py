847baf2ef44d6f811ec60f4c2ea17c76677d73f0
RitaFerraro <116449139+RitaFerraro@users.noreply.github.com>
2022-12-03 21:53:52 +0000

    Esercizio Lezione 6. Ci ho lavorato un sacco, non prendo piu' di 4. Inoltre gli errori di questa versione non li capisco!

--- lezione5b.py
+++ lezione5b.py
@@ -1,35 +1,41 @@
-class CSVFile:
+class CSVFile: 
     def __init__(self, name):
-        self.name = name
+            self.name = name
     def get_data(self):
         data = []
-        my_file = open(self.name, 'r')
-        for line in my_file:
-            elements = line.split(',')
-            if elements[0] != 'Date':
-                elements[1] = elements[1][0:-1]
-                data.append(elements)
-        my_file.close()
-        if len(data)==0:
-            return None
-        return data
+        try:
+            my_file = open(self.name, 'r')
+            for line in my_file:
+                elements = line.split(',')
+                if elements[0] != 'Date':
+                    elements[1] = elements[1][0:-1]
+                    data.append(elements)
+            my_file.close()
+            return data
+        except Exception:
+            print('Errore. Non posso  istanziare il mio oggetto visto che il file "{}" non esiste.'.format(self.name))  
 class NumericalCSVFile(CSVFile):
     def get_data(self):
         data = super().get_data()
         numerical_data=[]
-        for item in data:
+        if data != None:
             try:
-                item[1]=float(item[1])
-                numerical_data.append(item)
-            except ValueError:
-               print('Errore. Non posso convertire "item[1]" a valore numerico')
-               print('Ho avuto un errore di VALORE. "item[1]" valeva "{}"'.     format((item[1])))
-            except TypeError:
-                print('Errore. Non posso convertire "item[1]" a valore numerico')
-                print('Ho avuto un errore di TIPO. "item[1]" valeva "{}"'. format(type(item[1])))
-            except Exception as e:
-                print('Errore. Non posso convertire "item[1]" a valore numerico')
-                print('Ho avuto un errore generico. "{}"'. format(e))
+                for  item in data:
+                    for i in range(1, len(item)): 
+                        try:
+                            item[i]=float(item[i])
+                            numerical_data.append(item)
+                        except ValueError:
+                            print("Errore. Non posso convertire '{}'' a valore numerico.".format((item[1])))
+                            print('Ho avuto un errore di VALORE. "item[1]" valeva "{}"'.     format((item[1])))
+                        except TypeError:
+                            print('Errore. Non posso convertire "item[1]" a valore numerico')
+                            print('Ho avuto un errore di TIPO. "item[1]" valeva "{}"'. format(type(item[1])))
+                        except Exception as e:
+                            print('Errore. Non posso convertire "item[1]" a valore numerico')
+                            print('Ho avuto un errore generico. "{}"'. format(e))
+            except Exception:
+                print('Errore. Il file da cui voglio estrarre i dati numerici on esiste')
         return numerical_data
 
 
---
+++ lezione6.py
@@ -0,0 +1,160 @@
+#===================================================================
+#Esercizio Lezione 6
+#===================================================================
+
+#Modifico l'oggetto CSV file se il nome del file non e' una stringa
+class CSVFile: 
+    def __init__(self, name):
+        
+        #se name e' una stringa setto il nome del file altrimenti alzo un'eccezione
+        if type(name) is str:
+            self.name = name
+
+            #Inizializzo a vera una variabile nella classe per dire che il file e' leggibile, provo a leggere una riga
+            self.can_read = True
+            try:
+                my_file = open(self.name, 'r')
+                my_file.readline()
+            #Se non riesco a leggere una riga metto falsa la variabile e stampo un messaggio di errore                
+            except Exception as e:
+                self.can_read = False
+                print('Errore in apertura del file: "{}"'.format(e))
+                
+        else:
+            raise Exception('Ho avuto un errore. Il nome del file "{}" non e una stringa'.format(name) )
+    
+    #Modifico la funzione get_data in modo da leggere solo un intervallo di righe, per questo aggiungo gli argomenti start e end in modo opzionale, cioe' non devo essere obbligato ad inserirli, per   questo li inizializzo a None    
+    
+    def get_data(self, start=None, end=None):
+        start_type=type(start)
+        end_type=type(end)
+
+        # se non riuscivo a leggere il file esco
+        if not self.can_read:
+            print('Errore, il file e illeggibile')
+            return None
+
+        # se l'argomento start viene indicato ma non e' un intero esco:
+        if start is not None and start_type != int:
+            raise Exception('Errore. Errore dato dal parametro "{}"'.format(start))
+        
+        # se l'argomento end viene indicato ma non e' un intero esco:
+        if end is not None and end_type != int:
+            raise Exception('Errore. Errore dato dal parametro "{}"'.format(end))
+
+        #se l'argomento start e' un intero ma minore di 1, esco:
+        if start_type == int and start <1:
+            raise Exception('Errore. Errore dato dal parametro"{}"'.format(start))
+            
+        # se l'argomento end e' un intero ma minore di 1, esco
+        if end_type == int and end <1:
+            raise Exception('Errore. Errore dato dal parametro"{}"'.format (end))
+
+        #==================================================================
+        # se start e end non vengono indicati mi tengo la vecchia get_data
+        #==================================================================
+        if  start is None and end is None : 
+            data = []
+            my_file = open(self.name, 'r')
+            for line in my_file:
+                if not line:
+                    break
+                elements = line.split(',')
+                elements[-1]=elements[-1].strip()
+                if elements[0]!= 'Date':
+                    data.append(elements)
+    
+            my_file.close
+        #======================================================================================
+        # se viene indicato almeno uno tra start e end e sono numeri comincio ad analizzare i                casi:
+        #======================================================================================
+        else:
+            data = []
+            my_file = open(self.name, 'r')
+            file_lines= my_file.readlines()
+            #cerco di sapere quanto e' lungo il file
+            lunghezza= len(file_lines)
+            
+            # Ci sono sia start che end
+            if start_type == int and end_type == int and end-start>=1:
+                
+                #se start o end vanno oltre il numero di righe del file esco:
+                if start>=lunghezza or end>lunghezza:
+                    my_file.close()
+                    raise Exception('intervallo di righe da leggere non e compatibile con il numero di righe del file')
+                else:
+                    for i in range(start, end+1):
+                        try:
+                            my_file.readline()
+                            line=my_file.readline()
+                        except Exception as e:
+                            self.can_read = False
+                        
+                        if not self.can_read:
+                            my_file.close()
+                            raise Exception('Riga non leggibile')
+                        else:
+                            if line!="":
+                                elements = line.split(',')
+                                elements[-1]=elements[-1].strip()
+                                data.append(elements)
+                            else:
+                                my_file.close()
+                                break
+            #c'e' o solo end o solo start                   
+            else:
+                if start is None and end_type == int:
+                    
+                    # non voglio che end sia maggiore del numero di righe del file
+                    if end> lunghezza:
+                        my_file.close()
+                        raise Exception('Errore. Mi chiedi di leggere oltre la lunghezza del file')
+                    else:
+                        for i in range(1, end+1):
+                            line=my_file.readline()
+                            if line!= "":
+                                elements = line.split(',')
+                                elements[-1]=elements[-1].strip()
+                                data.append(elements)
+                            else:
+                                my_file.close()
+                                break
+                if start_type == int and end is None: 
+
+                    # non voglio che start sia maggiore o uguale al numero di righe del file
+                    if start>=lunghezza:
+                        my_file.close()
+                        raise Exception('Errore.Start inizia dopo la fine del file')
+                    else:
+                        i=1
+                        while i>=start:
+                            try:
+                                line=my_file.readline()
+                            except  Exception:
+                                self.can_read=False
+                            
+                            if not self.can_read:
+                                raise Exception('Riga illeggibile')
+                            else:
+                                if line!="":
+                                    elements = line.split(',')
+                                    elements[-1]=elements[-1].strip()
+                                    data.append(elements)
+                                    i=i+1
+                                else:
+                                    my_file.close()
+                                    break
+
+                #se l'argomento end e' minore di start esco:
+                if start_type == int and end_type == int and end-start <= 0:
+                    raise Exception('Errore')
+                    
+            my_file.close()
+
+           
+        return data
+        
+
+#Shampoo = NumericalCSVFile('shampoo_sales.csv')
+#Shampoo_data=Shampoo.get_data()
+#print(Shampoo_data)
